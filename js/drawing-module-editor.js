09c5cb8519a0c1b9fba47f05bbb7950b20ccd013
AbhijitParate <abhijitpparate@gmail.com>
2016-09-15 19:27:45 -0700

    Update 6 : September 17
    * Added Color pallet
    
    * Added user defined image upload dialog
    
    * Added preview of selected image
    
    * Added sample templates for demo purpose
    
    * Preview of user selected image from device
    
    * Fixed Zoom and Stroke-size slider

--- js/drawing-module-editor.js
+++ js/drawing-module-editor.js
@@ -5,42 +5,49 @@ $(document).ready(function(){
 
     var lc = LC.init(document.getElementsByClassName('drawing-module-canvas')[0], {backgroundColor : '#fff'});
 
+    // This is to disable right click on canvas
     $(".drawing-module-canvas").contextmenu(function (e) {
         e.preventDefault();
     });
 
+    // to save canvas content as JPEG image
     $("#save-image").click(function () {
         window.open(lc.getImage().toDataURL());
     });
 
-    $("#camera").click(function () {
-        $("#dialog-insert").dialog( "open" );
-        Webcam.attach("#front-cam");
-    });
 
-    $("#template").click(function () {
-        Webcam.reset();
-    });
 
-    var webcamImage ;
+
+    // $("#template").click(function () {
+    //     Webcam.reset();
+    // });
+
+
+    var uploadImage = new Image() ;
 
     $("#capture").click(function () {
         Webcam.snap( function(data_uri) {
             document.getElementById('front-cam').innerHTML = '<img src="'+data_uri+'"/>';
-            webcamImage = data_uri;
+            uploadImage = data_uri;
         } );
     });
 
-    $("#tabs").tabs({
-        select: function() {
-            alert("PRESSED TAB!");
-        }
-    });
 
-    $("#dialog-insert").dialog({
+
+    // $("#tabs").tabs({
+    //     select: function() {
+    //         alert("PRESSED TAB!");
+    //     }
+    // });
+
+    // creates dialog on click of webcam button
+    $("#dialog-webcam").dialog({
         resizable: false,
         height: "auto",
-        width: 400,
+        width: "auto",
+        close: function () {
+            Webcam.reset();
+        },
         modal: true,
         autoOpen: false,
         buttons: {
@@ -48,7 +55,7 @@ $(document).ready(function(){
                 $( this ).dialog( "close" );
                 Webcam.reset();
                 var newImage = new Image();
-                newImage.src = webcamImage;
+                newImage.src = uploadImage;
                 lc.saveShape(LC.createShape('Image', {x: 10, y: 10, image: newImage}));
             },
             "Cancel": function() {
@@ -58,6 +65,77 @@ $(document).ready(function(){
         }
     });
 
+    // on click of file input button un upload dialog
+    $("#fileupload").change(function () {
+
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                // $('#blah').attr('src', e.target.result);
+                // uploadImage = new Image();
+                uploadImage = e.target.result;
+                $("#preview-image").attr('src',uploadImage);
+                // lc.saveShape(LC.createShape('Image', {x: 10, y: 10, image: newImage}));
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+
+    });
+
+
+
+    // creates dialog on click of upload button
+    $("#dialog-upload").dialog({
+        resizable: false,
+        height: "auto",
+        width: "auto",
+        modal: true,
+        autoOpen: false,
+        buttons: {
+            "OK": function() {
+                $( this ).dialog( "close" );
+                var newImage = new Image();
+                newImage.src = uploadImage;
+                lc.saveShape(LC.createShape('Image', {x: 10, y: 10, image: newImage}));
+            },
+            "Cancel": function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+
+    // Color pallet
+    $("#colorpicker").spectrum({
+        showPaletteOnly: true,
+        togglePaletteOnly: true,
+        togglePaletteMoreText: 'more',
+        togglePaletteLessText: 'less',
+        color: 'black',
+        palette: [
+            ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+            ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+            ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
+            ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+            ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+            ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+            ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+            ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+        ],
+        change: function(color) {
+            lc.setColor('primary', color);
+        }
+    });
+
+    $("#black").click(function () {
+        $("#colorpicker").spectrum("show");
+    });
+
+
+    $("#template-select").change(function () {
+        $("#preview-image").attr('src',"images/templates/"+$(this).val()+".jpg");
+    });
+
     $("#print").click(function () {
         var imgData = lc.getImage().toDataURL("image/jpeg");
         var pdf = new jsPDF('landscape');
@@ -65,26 +143,14 @@ $(document).ready(function(){
         pdf.save("download.pdf");
     });
 
-    $("#upload").click(function () {
-        $("#imagefile").click();
+    // create dialog on click of camera button
+    $("#camera").click(function () {
+        $("#dialog-webcam").dialog( "open" );
+        Webcam.attach("#front-cam");
     });
 
-
-    $("#imagefile").change(function () {
-
-        if (this.files && this.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                // $('#blah').attr('src', e.target.result);
-                var newImage = new Image();
-                newImage.src = e.target.result;
-                lc.saveShape(LC.createShape('Image', {x: 10, y: 10, image: newImage}));
-            };
-
-            reader.readAsDataURL(this.files[0]);
-        }
-
+    $("#upload").click(function () {
+        $("#dialog-upload").dialog( "open" );
     });
 
 
@@ -176,10 +242,6 @@ $(document).ready(function(){
 
     var colors = [
         {
-            name: 'black',
-                el: document.getElementById('black'),
-            color: 'black'
-        },{
             name: 'blue',
                 el: document.getElementById('blue'),
                 color: 'blue'
@@ -217,7 +279,7 @@ $(document).ready(function(){
         };
     });
 
-    activateColors(colors[0]);
+    // activateColors(colors[0]);
 
     $("#size").change(function(e) {
         lc.trigger('setStrokeWidth', parseInt($(e.currentTarget).val(), 10));
