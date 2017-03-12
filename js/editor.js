408fbbeb36625f7354cbab777ca4c282045d1a7b
AbhijitParate <abhijitpparate@gmail.com>
2017-03-12 03:52:38 -0700

    Updated files and links

--- js/editor.js
+++ js/editor.js
@@ -1,190 +1,101 @@
-/**
- * Created by Abhijit on 6/15/16.
+
+/*
+ * Created by abhijit on 12/7/16.
  */
 
+$(document).ready(function() {
 
+    // Image
+    fabric.Object.prototype.setOriginToCenter = function () {
+        this._originalOriginX = this.originX;
+        this._originalOriginY = this.originY;
 
+        let center = this.getCenterPoint();
 
-(function($) {
+        this.set({
+            originX: 'center',
+            originY: 'center',
+            left: center.x,
+            top: center.y
+        });
+    };
 
-    // Invoke function once the document is fully loaded
-    window.addEventListener('load', init, false);
+    fabric.Object.prototype.setCenterToOrigin = function () {
+        let originPoint = this.translateToOriginPoint(
+            this.getCenterPoint(),
+            this._originalOriginX,
+            this._originalOriginY);
 
-    function init() {
-        var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d');
+        this.set({
+            originX: this._originalOriginX,
+            originY: this._originalOriginY,
+            left: originPoint.x,
+            top: originPoint.y
+        });
+    };
 
-        // flag for mouse down or up
-        var down = false;
+    // Edit
+    // canvas.counter = 0;
+    let newleft = 0;
 
-        // flag for maintaining tool in use currently
-        var toolInUse = null;
-
-        context.strokeStyle = 'black';
-        context.strokeStyle = 'round';
-
-        // onClick listeners
-        // Group DRAW
-        document.querySelector('#pen').onclick = function () {
-            toolInUse = 'pen';
-            // alert(toolInUse);
-        };
-        document.querySelector('#erase').onclick = function () {
-            toolInUse = 'erase';
-        };
-
-        // onClick listeners for clear Canvas
-        // Group COLOR
-        document.querySelector('#black').onclick = function () {
-            context.strokeStyle = "black";
-        };
-        document.querySelector('#red').onclick = function () {
-            context.strokeStyle = "red";
-        };
-        document.querySelector('#green').onclick = function () {
-            context.strokeStyle = "green";
-        };
-        document.querySelector('#blue').onclick = function () {
-            context.strokeStyle = "blue";
-        };
-        document.querySelector('#yellow').onclick = function () {
-            context.strokeStyle = "yellow";
-        };
-
-        // onClick listeners
-        // Group EDIT
-
-        document.querySelector('#clear').onclick = function () {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-        };
-        document.querySelector('#hide').onclick = function () {
-
-        };
-        // onClick listeners
-        // Group SIZE
-        document.querySelector('#size').onclick = function () {
-            context.lineWidth = document.getElementById('size').value;
-        };
-
-
-        canvas.addEventListener('mousemove', draw);
-
-        function draw(event) {
-            // x = event.clientX - canvas.getBoundingClientRect().left;
-            // y = event.clientY - canvas.getBoundingClientRect().top;
-            var x = event.clientX - canvas.offsetLeft;
-            var y = event.clientY - canvas.offsetTop;
-
-            if (toolInUse == 'pen') {
-                if (down == true) {
-                    context.lineTo(x, y);
-                    context.stroke();
-                }
-            }
-            if (toolInUse == 'shape') {
-                
-            }
-
-
+    let canvasState = [];
+    let mods = 0;
+    // canvas.on('object:modified', function () {
+    //     updateModifications(true);
+    // });
+    // canvas.on('object:added', function () {
+    //     updateModifications(true);
+    // });
+    function updateModifications(savehistory) {
+        if (savehistory === true) {
+            let myjson = JSON.stringify(canvas.toDatalessJSON());
+            canvasState.push(myjson);
+            mods++;
         }
-
-        // function changeToolInUse(tool) {
-        //     toolInUse = tool;
-        //     // alert(toolInUse);
-        // }
-        //
-        // function changeColor(color) {
-        //     context.strokeStyle = color;
-        // }
-
-        // onClick listeners to change color in use
-        canvas.addEventListener('mousedown', function () {
-            down = true;
-            context.beginPath();
-            context.moveTo(x, y);
-            canvas.addEventListener("mousemove", draw);
-        });
-
-        canvas.addEventListener('mouseup', function () {
-            down = false;
-        });
-
-        // var history = {
-        //     redo_list: [],
-        //     undo_list: [],
-        //     saveState: function(canvas, list, keep_redo) {
-        //         keep_redo = keep_redo || false;
-        //         if(!keep_redo) {
-        //             this.redo_list = [];
-        //         }
-        //
-        //         (list || this.undo_list).push(canvas.toDataURL());
-        //     },
-        //     undo: function(canvas, ctx) {
-        //         this.restoreState(canvas, ctx, this.undo_list, this.redo_list);
-        //     },
-        //     redo: function(canvas, ctx) {
-        //         this.restoreState(canvas, ctx, this.redo_list, this.undo_list);
-        //     },
-        //     restoreState: function(canvas, ctx,  pop, push) {
-        //         if(pop.length) {
-        //             this.saveState(canvas, push, true);
-        //             var restore_state = pop.pop();
-        //             var img = new Element('img', {'src':restore_state});
-        //             img.onload = function() {
-        //                 ctx.clearRect(0, 0, 600, 400);
-        //                 ctx.drawImage(img, 0, 0, 600, 400, 0, 0, 600, 400);
-        //             }
-        //         }
-        //     }
-        // }
-
-        // function setMousePosition(e) {
-        //     var ev = e || window.event; //Moz || IE
-        //     if (ev.pageX) { //Moz
-        //         mouse.x = ev.pageX + window.pageXOffset;
-        //         mouse.y = ev.pageY + window.pageYOffset;
-        //     } else if (ev.clientX) { //IE
-        //         mouse.x = ev.clientX + document.body.scrollLeft;
-        //         mouse.y = ev.clientY + document.body.scrollTop;
-        //     }
-        // }
-        //
-        // var mouse = {
-        //     x: 0,
-        //     y: 0,
-        //     startX: 0,
-        //     startY: 0
-        // };
-        // var element = null;
-        //
-        // canvas.onmousemove = function (e) {
-        //     setMousePosition(e);
-        //     if (element !== null) {
-        //         element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-        //         element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-        //         element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-        //         element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-        //     }
-        // };
-        //
-        // canvas.onclick = function (e) {
-        //     if (element !== null) {
-        //         element = null;
-        //         canvas.style.cursor = "default";
-        //         console.log("finished.");
-        //     } else {
-        //         console.log("begun.");
-        //         mouse.startX = mouse.x;
-        //         mouse.startY = mouse.y;
-        //         element = document.createElement('div');
-        //         element.className = 'rectangle';
-        //         element.style.left = mouse.x + 'px';
-        //         element.style.top = mouse.y + 'px';
-        //         canvas.appendChild(element);
-        //     }
-        // };
     }
 
-}); // Self invoking function
+    function undo() {
+        if (mods < canvasState.length) {
+            canvas.clear().renderAll();
+            canvas.loadFromJSON(canvasState[canvasState.length - 1 - mods - 1], canvas.renderAll.bind(canvas), null);
+            canvas.renderAll();
+            //console.log("geladen " + (state.length-1-mods-1));
+            //console.log("state " + state.length);
+            mods += 1;
+            //console.log("mods " + mods);
+        }
+    }
 
+    function redo() {
+        if (mods > 0) {
+            canvas.clear().renderAll();
+            canvas.loadFromJSON(canvasState[canvasState.length - 1 - mods + 1], canvas.renderAll.bind(canvas), null);
+            canvas.renderAll();
+            //console.log("geladen " + (state.length-1-mods+1));
+            mods -= 1;
+            //console.log("state " + state.length);
+            //console.log("mods " + mods);
+        }
+    }
+
+    // let uploadImage = new Image();
+    // let attachedImage = new Image();
+
+
+    function addFileBlobToAttachments(name, data, type) {
+        var fileReader = new FileReader();
+        fileReader.onloadend = function (e) {
+            var arrayBuffer = e.target.result;
+            var fileType = image.type;
+            blobUtil.arrayBufferToBlob(arrayBuffer, fileType)
+                .then(function (blob) {
+                    console.log('here is a blob', blob);
+                    console.log('its size is', blob.size);
+                    console.log('its type is', blob.type);
+                    var attachment = new Attachment(name, type , blob );
+                    attachments.push(attachment);
+                }).catch(console.log.bind(console));
+        };
+        fileReader.readAsArrayBuffer(data);
+    }
+} );
---
+++ js/status_actions.js
@@ -0,0 +1,201 @@
+/**
+ * Created by abhij on 3/7/2017.
+ *
+ */
+
+$(document).ready(function () {
+
+    var progressLabel, progressBar;
+
+    /*
+    if(pageMode == "view"){
+        $("#status-save").hide();
+        loadDataFromEncounter();
+    } else if (pageMode == "edit"){
+        loadDataFromEncounter();
+    }
+    */
+
+    var previousAttachments;
+
+    function loadDataFromEncounter() {
+        console.debug("EcnounterId : " + encounterId);
+
+        $.getJSON(emr.fragmentActionLink("annotation", "drawingDetails", "getEncounterDetails", { encounterId: encounterId }),
+            function success(data) {
+                recreateCanvas(data.drawing);
+                if(data.obs.length > 0) {
+                    previousAttachments = data.obs;
+                }
+            })
+            .fail(function() {
+                console.log( "error" );
+                emr.errorMessage("Failed to load Encounter!");
+            })
+            .always(function() {
+                console.log( "complete" );
+                emr.successMessage("Encounter loaded successfully!");
+            });
+    }
+
+    function recreateCanvas(drawing) {
+        console.debug(drawing);
+        fabric.loadSVGFromURL("../ws/rest/v1/annotation/obs/" + drawing.uuid +"/"+drawing.name, function(objects, options) {
+            var obj = fabric.util.groupSVGElements(objects, options);
+            canvas.add(obj).renderAll();
+        });
+    }
+
+    function recreateAttachments(obsArray) {
+        console.debug(obsArray);
+        var attachmentDiv = $("<div/>");
+        var list = $("<ul/>").addClass("attachments-list");
+
+        obsArray.forEach(function (obs) {
+            list.append(createListItem(obs));
+        });
+        attachmentDiv.append(list);
+        return attachmentDiv;
+    }
+
+    function createListItem(obs) {
+        var a = $("<a/>").addClass("attachments-list-item")
+            .attr("href", "../ws/rest/v1/annotation/obs/" + obs.uuid +"/"+obs.name)
+            .attr("title", obs.name)
+            .text(obs.name)
+            .css({ display: "block" })
+            .lightcase();
+        return $("<li/>").append(a);
+    }
+
+    // Show list of attachments on click
+    $("#status-view-attachments").click(function () {
+        if(previousAttachments){
+            var dom = recreateAttachments(previousAttachments);
+            $("<div/>").attr("title", "Attachments").append(dom).dialog({
+                closeText: "hide"
+            }).dialog( "open" );
+        } else {
+            $("#dialog-attachment").dialog("open");
+        }
+    });
+
+    // Show list of attachments on click
+    $("#status-save").click(function () {
+        // uploadDialog.dialog("open");
+        // $("<p/>").append("Uploading");
+        // $("#drawing-and-attachments").hide();
+        $(this).prop('disabled', true);
+        $("#status-cancel").prop('disabled', true);
+        $("#status-view-attachments").prop('disabled', true);
+        $("#main-container").hide();
+        progressLabel = $("<p/>");
+        progressBar = $("<div/>").progressbar({
+            max: attachments.length,
+            value: false,
+            change: function() {
+                progressLabel.text( "Saving : " + progressBar.progressbar( "value" ) + "%" );
+            },
+            complete: function() {
+                progressLabel.text( "Complete!" );
+            }
+        });
+        $("#progress-container").append(progressLabel).append(progressBar).show();
+        saveData();
+    });
+
+    $("#status-cancel").click(function () {
+        window.location.href = returnlink;
+        return false;
+    });
+
+    var formData = new FormData();
+
+    function saveData() {
+        prepareForm();
+        saveSVG(formData);
+        saveAttachments(formData);
+        savePreviousObs();
+        uploadData();
+    }
+
+    function prepareForm() {
+        formData.append("patientid", patientid);
+        formData.append("visitid", visitid);
+        formData.append("providerid", providerid);
+    }
+
+    function saveSVG() {
+        formData.append("files[]", btoa(canvas.toSVG()));
+        formData.append("filenames[]", Math.floor(Date.now()) + ".svg" );
+    }
+
+    function saveAttachments() {
+        for (let i = 0; i < attachments.length; i++) {
+            var attachedFile = attachments[i];
+            formData.append("files[]", attachedFile.data);
+            formData.append("filenames[]", attachedFile.name);
+            progressBar.progressbar( "value", i);
+        }
+    }
+
+    function savePreviousObs() {
+        if(previousAttachments) {
+            for (let i = 0; i < previousAttachments.length; i++) {
+                var obs = previousAttachments[i];
+                formData.append("obs[]", obs.uuid);
+            }
+        }
+    }
+
+    function uploadData() {
+        //new ajax request
+        let request = new XMLHttpRequest();
+
+        //event listener progress
+        request.upload.addEventListener('progress',function(event){
+            if(event.lengthComputable){
+                //get our percentage
+                var percent = (Math.round(event.loaded / event.total) * 100);
+                console.error( " progress: " + percent + " %");
+                // progressBar.progressbar( "value", percent);
+            }
+        });
+
+        //add event for when the progress is done
+        request.upload.addEventListener('load',function(data){
+
+        });
+
+        //for errors we'll use the info element but for now console log it
+        request.upload.addEventListener('error',function(event){
+            progressLabel.text("Error occurred!");
+            console.log("error: " + event);
+        });
+
+        //open the request
+        request.open("POST","upload.form");
+
+        //set the request header for no caching
+        request.setRequestHeader("Cache-Control","no-cache");
+
+        //send the data
+        request.send(formData);
+
+        request.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // progressBar.hide();
+                console.info( "success: Upload succeeded");
+                console.info( "success: Upload response : " + request.responseText);
+                if(request.responseText.includes("success")) {
+                    progressLabel.text("Encounter saved successfully!");
+                    window.location.href = returnlink;
+                    emr.successMessage("Encounter saved successfully!");
+                } else {
+                    progressLabel.text("Failed to save Encounter!");
+                    emr.errorMessage("Failed to save Encounter!");
+                }
+            }
+        };
+    }
+});
